00e09a61413adc96dbf5c00b32774868449ff9f7
lironbrantz <52230166+lironbrantz@users.noreply.github.com>
2026-09-26 11:22:33 +0300

    add book service and demo books

---
+++ miss-book/services/book-service.js
@@ -0,0 +1,75 @@
+import { utilService } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+const BOOK_KEY = 'bookDB'
+_createBooks()
+
+export const bookService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyBook,
+    getDefaultFilter,
+}
+
+function query(filterBy = {}) {
+    return storageService.query(BOOK_KEY)
+        .then(books => {
+            if (filterBy.title) {
+                const regExp = new RegExp(filterBy.title, 'i')
+                books = books.filter(book => regExp.test(book.title))
+            }
+
+            if (filterBy.price) {
+                books = books.filter(book => book.listPrice <= filterBy.price)
+            }
+
+            return books
+        })
+}
+
+function get(bookId) {
+    return storageService.get(BOOK_KEY, bookId)
+}
+
+function remove(bookId) {
+    // return Promise.reject('Oh No!')
+    return storageService.remove(BOOK_KEY, bookId)
+}
+
+function save(book) {
+    if (book.id) {
+        return storageService.put(BOOK_KEY, book)
+    } else {
+        return storageService.post(BOOK_KEY, book)
+    }
+}
+
+function getEmptyBook(title = '', listPrice = 0) {
+    return { title, listPrice }
+}
+
+function getDefaultFilter() {
+    return { title: '', price: '' }
+}
+
+function _createBooks() {
+    let books = utilService.loadFromStorage(BOOK_KEY)
+
+    if (!books || !books.length) {
+        books = [
+            _createBook('Harry Potter', 80),
+            _createBook('The Hobbit', 65),
+            _createBook('1984', 50),
+        ]
+
+        utilService.saveToStorage(BOOK_KEY, books)
+    }
+}
+
+function _createBook(title, listPrice = 0) {
+    const book = getEmptyBook(title, listPrice)
+    book.id = utilService.makeId()
+    return book
+}
